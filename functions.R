675ab12fe231f52acb03853468a8ce5f169b9f95
Chelsy Xie <cxie@wikimedia.org>
2017-07-13 09:53:33 -0700

    parse extraParams

--- functions.R
+++ functions.R
@@ -12,3 +12,27 @@ query_score <- function(positions, F) { # 0-based positions
 safe_ordinals <- function(x) {
   return(vapply(x, toOrdinal::toOrdinal, ""))
 }
+
+# Parse extraParams
+parse_extraParams <- function(extraParams, action){
+  if (extraParams == "{}") {
+    if (all(action %in% c("hover-on", "hover-off"))){
+      return(list(hoverId = NA, section = NA, results = NA))
+    } else if (all(action %in% c("esclick"))){
+      return(list(hoverId = NA, section = NA, result = NA))
+    } else if (all(action %in% c("searchResultPage"))){
+      return(list(offset = NA, iw = list(source = NA, position = NA)))
+    } else{
+      return(NA)
+    }
+  } else{
+    if (all(action %in% c("searchResultPage"))) {
+      output <- jsonlite::fromJSON(txt=as.character(extraParams))
+      offset <- if(is.null(output$offset)) NA else output$offset
+      iw <- if(is.null(output$iw)) list(source = NA, position = NA) else output$iw
+      return(list(offset = offset, iw = iw))
+    } else{ # "hover-on", "hover-off", "esclick"
+      return(jsonlite::fromJSON(txt=as.character(extraParams)))
+    }
+  }
+}
