7e85300d09982d4c79f31c24b9e5423226028cfd
Mikhail Popov <mpopov@wikimedia.org>
2017-07-21 17:02:49 -0700

    Code Review 1A
    Uploading these changes before the weekend. This commit includes:
    - Ignore certain files/directories
    - Syntax/style fixes for legibility & uniformity
    - Optimizations (e.g. switching to purrr)
    - ggplot2 theme `theme_min()`
    - Links
    - PaulScore definition

--- functions.R
+++ functions.R
@@ -28,23 +28,29 @@ safe_ordinals <- function(x) {
 # Parse extraParams
 parse_extraParams <- function(extraParams, action){
   if (extraParams == "{}") {
-    if (all(action %in% c("hover-on", "hover-off"))){
+    if (all(action %in% c("hover-on", "hover-off"))) {
       return(list(hoverId = NA, section = NA, results = NA))
-    } else if (all(action %in% c("esclick"))){
+    } else if (all(action %in% c("esclick"))) {
       return(list(hoverId = NA, section = NA, result = NA))
-    } else if (all(action %in% c("searchResultPage"))){
+    } else if (all(action %in% c("searchResultPage"))) {
       return(list(offset = NA, iw = list(source = NA, position = NA)))
-    } else{
+    } else {
       return(NA)
     }
-  } else{
+  } else {
     if (all(action %in% c("searchResultPage"))) {
-      output <- jsonlite::fromJSON(txt = as.character(extraParams))
-      offset <- if (is.null(output$offset)) NA else output$offset
-      iw <- if (is.null(output$iw)) list(source = NA, position = NA) else output$iw
+      output <- jsonlite::fromJSON(txt = as.character(extraParams), simplifyVector = TRUE)
+      offset <- polloi::data_select(is.null(output$offset), NA, output$offset)
+      iw <- polloi::data_select(is.null(output$iw), list(source = NA, position = NA), output$iw)
       return(list(offset = offset, iw = iw))
-    } else{ # "hover-on", "hover-off", "esclick"
-      return(jsonlite::fromJSON(txt = as.character(extraParams)))
+    } else {
+      # "hover-on", "hover-off", "esclick"
+      return(jsonlite::fromJSON(txt = as.character(extraParams), simplifyVector = TRUE))
     }
   }
+}
+
+theme_min <- function(base_family = "") {
+  ggplot2::theme_minimal(base_size = 12, base_family = base_family) +
+    ggplot2::theme(legend.position = "bottom", strip.placement = "outside")
 }
